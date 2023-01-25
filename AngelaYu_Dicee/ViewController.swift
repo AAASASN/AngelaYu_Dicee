de653b35d354ffd615f5a7f1501487bf9f5f0112
AAASASN <maraenko@gmail.com>
2023-01-26 01:47:29 +0300

    some commit

--- AngelaYu_Dicee/ViewController.swift
+++ AngelaYu_Dicee/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+    var diceArray = [UIImage(named: "DiceFive"),
+                     UIImage(named: "DiceFour"),
+                     UIImage(named: "DiceOne"),
+                     UIImage(named: "DiceSix"),
+                     UIImage(named: "DiceThree"),
+                     UIImage(named: "DiceTwo")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.layer.cornerRadius = 10
+        diceImageViewTwo.layer.cornerRadius = 10
+        buttonOutlet.layer.cornerRadius = 10
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+    }
+    
 }
 
