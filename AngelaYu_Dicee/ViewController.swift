7da6c1b50d40276e8788a0f519a5c187b9651936
AAASASN <maraenko@gmail.com>
2023-01-25 23:48:00 +0300

    init commit

---
+++ AngelaYu_Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AngelaYu_Dicee
+//
+//  Created by Александр Мараенко on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
